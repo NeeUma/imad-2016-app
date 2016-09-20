3ad618a9a059a95dedcfe6025dce987d0698ca2a
NeeUma <neepa@titan.co.in>
2016-09-20 23:56:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article1', function (req, res) {
 });
 
 app.get('/article2', function (req, res) {
-  res.send("in article2");
+  res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
